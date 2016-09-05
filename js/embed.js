06d1224da422aab35f151c3a128b1574cb768675
bchartoff <bchartoff@gmail.com>
2016-09-05 18:09:53 -0500

    chrome m53 dropdown bug

--- js/embed.js
+++ js/embed.js
@@ -955,20 +955,20 @@ d3.select("#mobileClose")
   .on("click", function(){
     d3.selectAll(".mobileHide").style("display", "none")
   })
-$(".styled-select.filter").click(function () {
-    var element = $(this).children("select")[0],
-        worked = false;
-    if(document.createEvent) { // all browsers
-        var e = document.createEvent("MouseEvents");
-        e.initMouseEvent("mousedown", true, true, window, 0, 0, 0, 0, 0, false,       false, false, false, 0, null);
-        worked = element.dispatchEvent(e);
-    } else if (element.fireEvent) { // ie
-        worked = element.fireEvent("onmousedown");
-    }
-    if (!worked) { // unknown browser / error
-        alert("It didn't worked in your browser.");
-    }
-});
+// $(".styled-select.filter").click(function () {
+//     var element = $(this).children("select")[0],
+//         worked = false;
+//     if(document.createEvent) { // all browsers
+//         var e = document.createEvent("MouseEvents");
+//         e.initMouseEvent("mousedown", true, true, window, 0, 0, 0, 0, 0, false,       false, false, false, 0, null);
+//         worked = element.dispatchEvent(e);
+//     } else if (element.fireEvent) { // ie
+//         worked = element.fireEvent("onmousedown");
+//     }
+//     if (!worked) { // unknown browser / error
+//         alert("It didn't worked in your browser.");
+//     }
+// });
 
 d3.selectAll(".styled-select.filter select")
   // .style("color", function(){
--- js/main.js
+++ js/main.js
@@ -1094,20 +1094,20 @@ d3.select(".hideChart")
       d3.select("#main-text").style("margin-top","-120px")
     }
   })
-$(".styled-select.filter").click(function () {
-    var element = $(this).children("select")[0],
-        worked = false;
-    if(document.createEvent) { // all browsers
-        var e = document.createEvent("MouseEvents");
-        e.initMouseEvent("mousedown", true, true, window, 0, 0, 0, 0, 0, false,       false, false, false, 0, null);
-        worked = element.dispatchEvent(e);
-    } else if (element.fireEvent) { // ie
-        worked = element.fireEvent("onmousedown");
-    }
-    if (!worked) { // unknown browser / error
-        alert("It didn't worked in your browser.");
-    }
-});
+// $(".styled-select.filter").click(function () {
+//     var element = $(this).children("select")[0],
+//         worked = false;
+//     if(document.createEvent) { // all browsers
+//         var e = document.createEvent("MouseEvents");
+//         e.initMouseEvent("mousedown", true, true, window, 0, 0, 0, 0, 0, false,       false, false, false, 0, null);
+//         worked = element.dispatchEvent(e);
+//     } else if (element.fireEvent) { // ie
+//         worked = element.fireEvent("onmousedown");
+//     }
+//     if (!worked) { // unknown browser / error
+//         alert("It didn't worked in your browser.");
+//     }
+// });
 
 d3.selectAll(".styled-select.filter select")
   // .style("color", function(){
